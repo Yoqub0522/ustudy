7c71979e74c0896f5766deaad20cde783fb5d751
Yoqub0522 <axmedov.yoqub0522@gmail.com>
2025-05-28 20:59:55 +0500

    Update urls.py

--- asosiy/urls.py
+++ asosiy/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path,include
 
-from asosiy.settings import MEDIA_URL
+# from asosiy.settings import MEDIA_URL
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -13,5 +13,5 @@ urlpatterns = [
 
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# if settings.DEBUG:
+#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
